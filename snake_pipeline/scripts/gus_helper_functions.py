fix off by one error

snake_pipeline/scripts/gus_helper_functions.py
@@ -46,7 +46,6 @@ def genomestats(REFGENOMEFOLDER):
     '''
 
     refgenome = read_fasta(REFGENOMEFOLDER)
-    
     Genomelength = 0
     ChrStarts = []
     ScafNames = []
@@ -54,9 +53,6 @@ def genomestats(REFGENOMEFOLDER):
         ChrStarts.append(Genomelength) # chr1 starts at 0 in analysis.m
         Genomelength = Genomelength + len(record)
         ScafNames.append(record.id)
-    # close file
-    #refgenome.close() # biopy update SeqIO has no close attribute anymore.
-    # turn to np.arrys!
     ChrStarts = np.asarray(ChrStarts,dtype=int)
     Genomelength = np.asarray(Genomelength,dtype=int)
     ScafNames = np.asarray(ScafNames,dtype=object)
@@ -75,11 +71,11 @@ def p2chrpos(p, ChrStarts):
     '''
         
     # get chr and pos-on-chr
-    chromo = np.ones(len(p),dtype=int)
+    chromo = np.zeros(len(p),dtype=int)
     if len(ChrStarts) > 1:
         for i in ChrStarts[1:]:
-            chromo = chromo + (p > i) # when (p > i) evaluates 'true' lead to plus 1 in summation. > bcs ChrStarts start with 0...genomestats()
-        positions = p - ChrStarts[chromo-1] # [chr-1] -1 due to 0based index
+            chromo = chromo + (p >= i) # when (p > i) evaluates 'true' lead to plus 1 in summation. > bcs ChrStarts start with 0...genomestats()
+        positions = p - ChrStarts[chromo] # [chr-1] -1 due to 0based index
         chrpos = np.column_stack((chromo,positions))
     else:
         chrpos = np.column_stack((chromo,p))

snake_pipeline/testing/python_testing_suite/test_general_snakemake_helpers.py
@@ -33,7 +33,8 @@ class TestMyFunction(unittest.TestCase):
         self.assertEqual(len(gus.genomestats(test_genome_dir_full_gzipped)),3)
         # test that starts are correct
         chrstarts_correct=[0,4]
-        self.assertEqual(gus.genomestats(test_genome_dir_full)[0],np.array(chrstarts_correct))
+        self.assertEqual(gus.genomestats(test_genome_dir_full)[0][0],chrstarts_correct[0])
+        self.assertEqual(gus.genomestats(test_genome_dir_full)[0][1],chrstarts_correct[1])
         # test that total length of record is 8
         self.assertEqual(len(gus.genomestats(test_genome_dir_full)[1]),8)
         # test that scaf names are good
@@ -44,10 +45,18 @@ class TestMyFunction(unittest.TestCase):
         p=np.array([0,1,2,3,4,5,6,7])
         chrstarts_for_p2chrpos_one_chroms=np.array([0])
         chrstarts_for_p2chrpos_two_chroms=np.array([0,4])
+        chrstarts_for_p2chrpos_three_chroms=np.array([0,2,4])
+        chrstarts_for_p2chrpos_short_contigs=np.array([0,1])
         # test only one contig
         self.assertTrue(np.all(gus.p2chrpos(p,chrstarts_for_p2chrpos_one_chroms)[:,1]==p))
-        # test contigs
-        self.assertEqual(len(gus.p2chrpos(p,chrstarts_for_p2chrpos_two_chroms)[1,:]),4)
+        # test two ontigs
+        self.assertEqual(len(gus.p2chrpos(p,chrstarts_for_p2chrpos_two_chroms)),4)
+        self.assertEqual(np.sum(p2chrpos(p,chrstarts_for_p2chrpos_two_chroms)[:,0]==0),4)
+        self.assertEqual(np.sum(p2chrpos(p,chrstarts_for_p2chrpos_two_chroms)[:,0]==1),4)
+        # test three contigs
+        self.assertEqual(len(np.unique(gus.p2chrpos(p,chrstarts_for_p2chrpos_three_chroms)[:,0])),3)
+        # test short contigs
+        self.assertEqual(np.sum(p2chrpos(p,chrstarts_for_p2chrpos_short_contigs)[:,0]==0),1)
 
 if __name__ == '__main__':
     unittest.main() ## when run as script